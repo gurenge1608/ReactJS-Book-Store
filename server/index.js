1d2ade19ba41a6d7b82752d93e9edb55e25e8287
ThuanHCMUT <tieuho036@gmail.com>
2022-04-26 23:36:42 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://tieuho036:tieuho036@reactjs-nodejs-db.zho0s.mongodb.net/reactjs-nodejs-db?retryWrites=true&w=majority`,{
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+
+        console.log('MongoDB connected')
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1)
+    }
+}
+
+connectDB()
 
 const app = express()
 
